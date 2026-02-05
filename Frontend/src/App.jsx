0f5aac5c22616f3b45f065eb53bbce79b6659977
Afzal Myrah (2024) ZNAC209 <myrah.afzal.2024@live.rhul.ac.uk>
2026-02-05 16:46:58 +0000

    Created the customer login page #5

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div className="page">
+            <div className="login-box">
+                <h1>OAXACA</h1>
+                <h2>Customer Login</h2>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                <div className="row">
+                    <label>Name:</label>
+                    <input type="text" placeholder="Enter full name here" />
+                </div>
+
+                <div className="row">
+                    <select>
+                        <option value="">Select Table Number</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                </div>
+
+                <button>CONTINUE</button>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
