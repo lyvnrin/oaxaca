7423fbd1b893049730cfdbb5b28ba7bcc023f06e
Afzal Myrah (2024) ZNAC209 <myrah.afzal.2024@live.rhul.ac.uk>
2026-02-09 20:31:57 +0000

    Organise files into folder

--- Frontend/src/pages/main.jsx
+++ Frontend/src/pages/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './pages/CustomerLogin.css';
+import App from '../App.jsx';
+import './CustomerLogin.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
