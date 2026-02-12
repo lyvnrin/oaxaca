aa7500359508118c467b96170644ffe4569bc1a3
danujan <wmis094@live.rhul.ac.uk>
2026-02-12 14:49:15 +0000

    Add remove item feature to order summary #24

--- Frontend/src/OrderSummary.jsx
+++ Frontend/src/OrderSummary.jsx
@@ -11,20 +11,12 @@ function OrderSummary() {
 
   const total = item1Qty * item1Price + item2Qty * item2Price;
 
-  function decreaseItem1() {
-    if (item1Qty > 0) setItem1Qty(item1Qty - 1);
+  function removeItem1() {
+    setItem1Qty(0);
   }
 
-  function increaseItem1() {
-    setItem1Qty(item1Qty + 1);
-  }
-
-  function decreaseItem2() {
-    if (item2Qty > 0) setItem2Qty(item2Qty - 1);
-  }
-
-  function increaseItem2() {
-    setItem2Qty(item2Qty + 1);
+  function removeItem2() {
+    setItem2Qty(0);
   }
 
   return (
@@ -38,25 +30,23 @@ function OrderSummary() {
     >
       <h2>ORDER SUMMARY</h2>
 
-      <div style={{ marginBottom: "10px" }}>
-        <p>
-          ITEM 1 (£{item1Price}) &nbsp; Qty: {item1Qty}
-        </p>
-        <button onClick={decreaseItem1}>-</button>
-        <button onClick={increaseItem1} style={{ marginLeft: "8px" }}>
-          +
-        </button>
-      </div>
+      {item1Qty > 0 && (
+        <div>
+          <p>
+            ITEM 1 (£{item1Price}) &nbsp; Qty: {item1Qty}
+          </p>
+          <button onClick={removeItem1}>Remove</button>
+        </div>
+      )}
 
-      <div style={{ marginBottom: "10px" }}>
-        <p>
-          ITEM 2 (£{item2Price}) &nbsp; Qty: {item2Qty}
-        </p>
-        <button onClick={decreaseItem2}>-</button>
-        <button onClick={increaseItem2} style={{ marginLeft: "8px" }}>
-          +
-        </button>
-      </div>
+      {item2Qty > 0 && (
+        <div>
+          <p>
+            ITEM 2 (£{item2Price}) &nbsp; Qty: {item2Qty}
+          </p>
+          <button onClick={removeItem2}>Remove</button>
+        </div>
+      )}
 
       <p>
         <b>Total: £{total}</b>
