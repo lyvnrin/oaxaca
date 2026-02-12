198e3a49f8f6827f83653436d2a59dd87d040f31
danujan <wmis094@live.rhul.ac.uk>
2026-02-12 14:41:13 +0000

    Add quantity controls to order summary #23

--- Frontend/src/OrderSummary.jsx
+++ Frontend/src/OrderSummary.jsx
@@ -6,7 +6,26 @@ function OrderSummary() {
   const item1Price = 5;
   const item2Price = 3;
 
-  const total = item1Price + item2Price;
+  const [item1Qty, setItem1Qty] = useState(1);
+  const [item2Qty, setItem2Qty] = useState(1);
+
+  const total = item1Qty * item1Price + item2Qty * item2Price;
+
+  function decreaseItem1() {
+    if (item1Qty > 0) setItem1Qty(item1Qty - 1);
+  }
+
+  function increaseItem1() {
+    setItem1Qty(item1Qty + 1);
+  }
+
+  function decreaseItem2() {
+    if (item2Qty > 0) setItem2Qty(item2Qty - 1);
+  }
+
+  function increaseItem2() {
+    setItem2Qty(item2Qty + 1);
+  }
 
   return (
     <div
@@ -19,8 +38,25 @@ function OrderSummary() {
     >
       <h2>ORDER SUMMARY</h2>
 
-      <p>ITEM 1  £{item1Price}</p>
-      <p>ITEM 2  £{item2Price}</p>
+      <div style={{ marginBottom: "10px" }}>
+        <p>
+          ITEM 1 (£{item1Price}) &nbsp; Qty: {item1Qty}
+        </p>
+        <button onClick={decreaseItem1}>-</button>
+        <button onClick={increaseItem1} style={{ marginLeft: "8px" }}>
+          +
+        </button>
+      </div>
+
+      <div style={{ marginBottom: "10px" }}>
+        <p>
+          ITEM 2 (£{item2Price}) &nbsp; Qty: {item2Qty}
+        </p>
+        <button onClick={decreaseItem2}>-</button>
+        <button onClick={increaseItem2} style={{ marginLeft: "8px" }}>
+          +
+        </button>
+      </div>
 
       <p>
         <b>Total: £{total}</b>
@@ -34,7 +70,8 @@ function OrderSummary() {
         onChange={(e) => setNotes(e.target.value)}
       />
 
-      <br /><br />
+      <br />
+      <br />
 
       <button>CANCEL ORDER</button>
       <button>PLACE ORDER</button>
