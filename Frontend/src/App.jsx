fb0fb9b50baabaa866f4cbf412b478af1931ffdb
Lavanya <znac542@live.rhul.ac.uk>
2026-02-13 12:47:38 +0000

    Linked MenuItems page to 'Main' button in Main page (#1, #5)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CustomerLogin from "./pages/CustomerLogin";
 import Menu from "./pages/Menu";
+import MenuItems from "./pages/MenuItems.jsx";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<CustomerLogin />} />
                 <Route path="/menu" element={<Menu />} />
+                <Route path="/menuitems" element={<MenuItems />} />
             </Routes>
         </BrowserRouter>
     );
