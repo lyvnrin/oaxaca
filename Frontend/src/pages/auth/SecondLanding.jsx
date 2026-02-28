fd4bca9696598b8d5dbee2878b55b6c015f1d8b7
Afzal Myrah (2024) ZNAC209 <myrah.afzal.2024@live.rhul.ac.uk>
2026-02-28 15:28:43 +0000

    Added animated background to landing pages #48

--- Frontend/src/pages/auth/SecondLanding.jsx
+++ Frontend/src/pages/auth/SecondLanding.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import Grainient from '../../components/Grainient';
 import './Landing.css';
 
 function SecondLanding() {
@@ -7,6 +8,30 @@ function SecondLanding() {
 
     return (
         <div className="container">
+            <Grainient
+                color1="#781111"
+                color2="#b94609"
+                color3="#9a0e0e"
+                timeSpeed={0.25}
+                colorBalance={0}
+                warpStrength={1}
+                warpFrequency={3}
+                warpSpeed={1.5}
+                warpAmplitude={40}
+                blendAngle={0}
+                blendSoftness={0.1}
+                rotationAmount={500}
+                noiseScale={2}
+                grainAmount={0}
+                grainScale={2}
+                grainAnimated={false}
+                contrast={1.2}
+                gamma={1}
+                saturation={0.6}
+                centerX={-0.09}
+                centerY={0.05}
+                zoom={0.9}
+            />
             <div className="top-nav">
                 <span>ABOUT</span>
                 <span>CONTACT</span>
